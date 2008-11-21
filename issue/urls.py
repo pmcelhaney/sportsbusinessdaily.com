4a9d076eb4f0291d65a7743e7dda73c3b5f0e8c7
pmcelhaney <pmcelhaney@1898daed-0948-0a4f-9563-fc73d751f481>
2008-11-21 21:00:17 +0000

    Started the home page.
    git-svn-id: svn://infoserv08/sbd-django/trunk@4887 1898daed-0948-0a4f-9563-fc73d751f481

--- issue/urls.py
+++ issue/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^home/(?P<issue_date>\d{4}-\d{2}-\d{2})$', 'sbd.issue.views.home'),
     (r'^article/(?P<article_id>\d+)$', 'sbd.issue.views.single_article'),
     (r'^print-issue/(?P<issue_date>\d{4}-\d{2}-\d{2})$', 'sbd.issue.views.print_issue'),
 )
--- issue/views.py
+++ issue/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from models import Article, Issue, PrintIssue
 
+def home(request, issue_date):
+  issue = get_object_or_404(Issue, pk=issue_date)
+  articles = issue.article_set.all().order_by('section','headline')
+  return render_to_response('issue/home.html', {'issue': issue.printissue, 'articles': articles})
+  
 def single_article(request, article_id):
   a = get_object_or_404(Article, pk=article_id)
   return render_to_response('issue/article.html', {'article': a})
