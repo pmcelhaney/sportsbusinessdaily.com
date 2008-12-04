b8a475bcdad2b6eef35a00402e588834dee3973a
pmcelhaney <pmcelhaney@1898daed-0948-0a4f-9563-fc73d751f481>
2008-12-04 22:10:20 +0000

    Finished banner code -- almost. It puts everything in the "main" zone. But the way we're using banners I doubt that really matters.
    git-svn-id: svn://infoserv08/sbd-django/trunk@4938 1898daed-0948-0a4f-9563-fc73d751f481

--- issue/templatetags/banners.py
+++ issue/templatetags/banners.py
@@ -1,5 +1,7 @@
 import re
+import random
 from django import template
+from django.template import Template, Context
 
 register = template.Library()
 
@@ -7,17 +9,31 @@ register = template.Library()
 
 class BannerNode(template.Node):
   
-  def __init__(self, w, h):
+  def __init__(self, pos, w, h):
+    self.pos = pos
     self.width = w
     self.height = h
     
   def render(self, context):
-    return "[banner: %sx%s]" % (self.width, self.height)
+    raw_template = """
+      <!-- {{ width }}x{{ height }} banner at position "{{ pos }}" in zone "{{ zone }}" -->
+      <iframe width="{{ width }}" height="{{ height }}" src="http://ad.doubleclick.net/adi/bzj.sportsbizdaily/{{ zone }};pos={{ pos }};sz={{ width }}x{{ height }};ord={{ ord }}?" MARGINWIDTH="0" MARGINHEIGHT="0" HSPACE="0" VSPACE="0" FRAMEBORDER="0" SCROLLING="no" BORDERCOLOR="#000000">
+  		</iframe>      	
+    """
+    t = Template(raw_template)
+    c = Context({
+      'width': self.width,
+      'height': self.height,
+      'pos': self.pos,
+      'zone': 'main',
+      'ord': random.randrange(100000,999999),
+    })
+    return t.render(c)
     
 @register.tag
 def banner(parser, token):
-  matches = re.match(r".+ (?P<width>\d+)x(?P<height>\d+)",  token.contents)
-  return BannerNode(matches.group('width'), matches.group('height'))
+  matches = re.match(r"\w+\s+(?P<pos>\w+)\s+(?P<width>\d+)x(?P<height>\d+)",  token.contents)
+  return BannerNode(matches.group('pos'), matches.group('width'), matches.group('height'))
   
   
 #  <iframe width="#arguments.width#" height="#arguments.height#" src="http<cfif cgi.https eq "on">s</cfif>://ad.doubleclick.net/adi/bzj.sportsbizdaily/#adDepartment#;pos=#arguments.pos#;sz=#arguments.width#x#arguments.height#;ord=#random_shimmy#?" MARGINWIDTH="0" MARGINHEIGHT="0" HSPACE="0" VSPACE="0" FRAMEBORDER="0" SCROLLING="no" BORDERCOLOR="##000000">
