d532fa1d17cf22a4c7e15d3505303072c7973e23
pmcelhaney <pmcelhaney@1898daed-0948-0a4f-9563-fc73d751f481>
2008-12-08 21:48:51 +0000

    Added "also in this section" to article page.
    git-svn-id: svn://infoserv08/sbd-django/trunk@4958 1898daed-0948-0a4f-9563-fc73d751f481

--- issue/views.py
+++ issue/views.py
@@ -13,7 +13,8 @@ def single_article(request, article_id):
   article = get_object_or_404(Article, pk=article_id)
   issue = article.issue
   articles = issue.article_set.all().order_by('section','headline')
-  return render_to_response('issue/article.html', {'article': article, 'articles': articles, 'issue': issue.homepage })
+  articles_in_section = articles.filter(section=article.section)
+  return render_to_response('issue/article.html', {'article': article, 'articles': articles, 'issue': issue.homepage, 'articles_in_section': articles_in_section })
   
 def print_issue(request, issue_date):
   issue = get_object_or_404(Issue, pk=issue_date)
