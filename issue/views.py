395ffcd264beb72d49fa905cebf432c99b921090
pmcelhaney <pmcelhaney@1898daed-0948-0a4f-9563-fc73d751f481>
2008-12-09 20:53:07 +0000

    Added related companies.
    git-svn-id: svn://infoserv08/sbd-django/trunk@4964 1898daed-0948-0a4f-9563-fc73d751f481

--- issue/views.py
+++ issue/views.py
@@ -12,9 +12,16 @@ def home(request, issue_date):
 def single_article(request, article_id):
   article = get_object_or_404(Article, pk=article_id)
   issue = article.issue
-  articles = issue.article_set.all().order_by('section','headline')
-  articles_in_section = articles.filter(section=article.section)
-  return render_to_response('issue/article.html', {'article': article, 'articles': articles, 'issue': issue.homepage, 'articles_in_section': articles_in_section })
+  articles_in_issue = issue.article_set.all().order_by('section','headline')
+  return render_to_response('issue/article.html', {
+    'article': article, 
+    'articles': articles_in_issue, 
+    'issue': issue.homepage, 
+    'articles_in_section': articles_in_issue.filter(section=article.section), 
+    'related_articles_by_sport': Article.objects.exclude(id=article.id).filter(companies__in=article.companies.all()).distinct().order_by('issue', 'headline')[:5],
+    'related_articles_by_company': Article.objects.exclude(id=article.id).filter(sports__in=article.sports.all()).distinct().order_by('issue', 'headline')[:5],
+    
+  })
   
 def print_issue(request, issue_date):
   issue = get_object_or_404(Issue, pk=issue_date)
